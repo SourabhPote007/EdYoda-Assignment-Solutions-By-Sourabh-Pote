deb9815b1b334b527f8135216b44323e927e1434
Sourabh Anil Pote <110386969+SourabhPote007@users.noreply.github.com>
2023-07-16 15:32:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Payment from './components/payment/Payment';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Payment/>
+    </div>
+  );
+}
+
+export default App;
